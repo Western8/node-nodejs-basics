ff5a52db9c452059bf7538eddb36baf8c05a0523
Western8 <westernchrome@gmail.com>
2024-01-12 23:41:57 +0300

    feat: add create file func

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs/promises';
+
 const create = async () => {
-    // Write your code here 
+    const path = './files/fresh.txt';
+    
+    let fileExist = false;
+    try {
+        await fs.access(path);
+        fileExist = true;
+    } catch(err) {
+    }
+    if (fileExist) {
+        throw 'FS operation failed'
+    };
+    try {
+        await fs.writeFile(path, 'I am fresh and young');
+    } catch(err) {
+        console.error(err);
+    }
 };
 
 await create();
