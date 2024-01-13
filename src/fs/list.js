8e69b29939e5cdc91783217d636a63c1dc11aed7
Western8 <westernchrome@gmail.com>
2024-01-13 12:55:58 +0300

    feat: add func list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const pathSrc = __dirname.concat('/files/');
+    let files = [];
+    try {
+        files = await fs.readdir(pathSrc);
+    } catch (err) {
+        console.log(err);
+        throw 'FS operation failed';
+    }
+    files.forEach((item) => {
+        console.log(item);
+    });
 };
 
 await list();
