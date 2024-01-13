3f85ce60a604d1642a224f9c3d872a5a1145d85a
Western8 <westernchrome@gmail.com>
2024-01-13 13:02:26 +0300

    feat: add func read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathSrc = __dirname.concat('/files/fileToRead.txt');
+    try {
+        const cont = await fs.readFile(pathSrc);
+        console.log(cont.toString());
+    } catch (err) {
+        console.log(err);
+        throw 'FS operation failed';
+    }
 };
 
 await read();
