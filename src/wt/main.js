e25485369f060bbb06091dedb959c6b91ab2858a
Western8 <westernchrome@gmail.com>
2024-01-14 01:50:07 +0300

    feat: add worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,48 @@
+import { Worker } from 'node:worker_threads';
+import os from 'os';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpu = os.cpus().length;
+    const pathWorker = __dirname.concat('/worker.js');
+    const promises = [];
+
+    for (let i = 0; i < cpu; i++) {
+        const data = i + 10;
+        const worker = new Worker(pathWorker);
+        const promise = new Promise((resolve, reject) => {
+            worker.on('message', msg => {
+                resolve(msg.res);
+            });
+            worker.on('error', msg => {
+                reject();
+            });
+        });
+
+        worker.postMessage(data);
+        promises.push(promise);
+    }
+
+    Promise.allSettled(promises)
+        .then(res => {
+            const results = res.map(item => {
+                const result = {};
+                if (item.status === 'fulfilled') {
+                    result.status = 'resolved';
+                    result.data = item.value;
+                }
+                if (item.status === 'rejected') {
+                    result.status = 'error';
+                    result.data = null;
+                }
+                return result;
+            });
+            console.log('Results: ', results);
+            process.exit();
+        });
 };
 
 await performCalculations();
