33f19117cca7546fe3c96b3ed4302f7da258c768
Western8 <westernchrome@gmail.com>
2024-01-13 12:49:44 +0300

    feat: add func delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const pathSrc = __dirname.concat('/files/fileToRemove.txt');
+    try {
+        await fs.rm(pathSrc);
+    } catch(err) {
+        console.log(err);        
+        throw 'FS operation failed';
+    }
 };
 
 await remove();
