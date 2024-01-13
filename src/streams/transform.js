2fcff36ee0963c112dd4d981175a43f1a562e244
Western8 <westernchrome@gmail.com>
2024-01-13 19:17:48 +0300

    feat: add transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from 'node:stream';
+
+function reverseStr(str) {
+    return str.slice(0, -1).split('').reverse().join('').concat('\n');
+}
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, encoding, cb) {
+            cb(null, reverseStr(chunk.toString()));
+        }
+    })
+    process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
 await transform();
